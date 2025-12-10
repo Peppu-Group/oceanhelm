fff9a0b1e2981cc280ef169be0f4f36c4497c403
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2025-12-10 19:52:31 +0100

    fixed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,13 +14,18 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav align-items-center">
                         <li class="nav-item">
-                            <a class="nav-link fw-semibold px-3" href="/features">Our Offering</a>
+                            <a class="nav-link fw-semibold px-3" href="/features">Features</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-semibold px-3" href="/">Who We Are</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-semibold px-3" href="#testimonials">Testimonials</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle fw-semibold px-3" href="#" id="navbarDropdown"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Our Offering
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="/#testimonials">Testimonials</a></li>
+                                <li><a class="dropdown-item" href="/case-studies">Case Studies</a></li>
+                                <li><a class="dropdown-item" href="/industries">Industries</a></li>
+                            </ul>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link fw-semibold px-3" href="/pricing">Pricing</a>
